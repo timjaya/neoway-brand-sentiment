1df8673d1db6298673eb25b57ef2ccec31a4600c
TimKartawijaya <tim.kartawijaya@gmail.com>
2019-09-26 20:22:56 -0400

    reset

--- template/model_name/config.py
+++
@@ -1,8 +0,0 @@
-from os import path
-
-import model_name
-
-base_path = path.dirname(path.dirname(model_name.__file__))
-workspace_path = path.join(base_path, 'workspace')
-data_path = path.join(workspace_path, 'data')
-models_path = path.join(workspace_path, 'models')
