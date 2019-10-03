ad235b0647ae793c7306cc2d7bfddd231db455d1
TimKartawijaya <tim.kartawijaya@gmail.com>
2019-10-03 15:16:08 -0500

    added template for cleaning module

---
+++ analysis/example.py
@@ -0,0 +1,6 @@
+import preprocess.clean as clean
+
+if __name__ == "__main__":
+    df = 5
+    df = clean.main_clean(df)
+    print(df)
---
+++ analysis/preprocess/clean.py
@@ -0,0 +1,18 @@
+import nltk
+import re
+from nltk.corpus import stopwords
+# from contractions import contractions_dict
+
+def main_clean(df):
+
+        # INSERT OTHER STEPS HERE
+        df = example_step(df)
+        df = example_step2(df)
+
+        return df
+    
+# Example preprocessing steps, if new one created, insert above
+def example_step(df):
+    return df
+
+
