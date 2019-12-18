6c24328a1bd34d79440fbff48653bc13e9dc8425
TimKartawijaya <tim.kartawijaya@gmail.com>
2019-12-18 09:53:30 -0500

    added pre-trained ER Model

--- neoway_nlp/main.py
+++ neoway_nlp/main.py
@@ -3,11 +3,16 @@ import fire
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 from collections import defaultdict
 import spacy
 from .runPrediction import prediction
 from .spacy_train import run_training
+from .spacy_validate import evaluate_spacy
+import benepar
+import matplotlib.pyplot as plt
+import ast
+from scipy.stats import spearmanr
 
 
 
@@ -50,7 +55,9 @@ def preprocess(reviews, brandlist, sample_size=20000, validation_size=0.1,
     if verbose == 1:
       print("   ===> CONVERTING DATA FOR SPACY")
     train_data = []
-    for index, row in sample.iterrows():
+    print("LENGTH OF DATASET: ", len(sample))
+    for index, row in tqdm(sample.iterrows()):
+        # print(index)
         brands_tmp = []
         for brand in brandlist.word:
             text = row.text.lower()
@@ -64,7 +71,7 @@ def preprocess(reviews, brandlist, sample_size=20000, validation_size=0.1,
                     if brand not in ['place', 'restaurant', 'cafe', 'establishment', 'diner']:
                         brands_tmp.append((start_index, end_index, "PRODUCT"))
                     else:
-                        brands_tmp.append((start_index, end_index, "ESTABLISHMENT"))
+                        brands_tmp.append((start_index, end_index, "PRODUCT"))
 
                 start_index += len(brand)
         train_data.append((row.review_id, row.text, brands_tmp))
@@ -101,14 +108,16 @@ def train(**kwargs):
     which was build from generate() step. You should save your model
     binary into workspace/models directory.
     """
-    print("==> TRAINING YOUR MODEL!")
+    print("==> TRAINING YOUR SPACY MODEL!")
 
     # TODO: Load data from workspace/data
     # TODO: Save trained model to workspace/models
     run_training()
+
+    print("==> SPACY TRAINING COMPLETE!")
     
 
-def metadata(**kwargs):
+def metadata_spacy(**kwargs):
     """Generate metadata for model governance using testing!
     TODO: since sentiment analysis validation takes a while, should take this as an option
 
@@ -139,7 +148,147 @@ def metadata(**kwargs):
     # Metadata for Sentiment Analysis
     # Use predict from Sentiment Analysis
 
-    print("==> TESTING MODEL PERFORMANCE AND GENERATING METADATA")
+    print("==> TESTING SPACY MODEL PERFORMANCE AND GENERATING METADATA")
+
+    evaluate_spacy()
+
+def validate_end_to_end():
+
+    benepar.download("benepar_en2")
+    parser = benepar.Parser("benepar_en2")
+
+    example = "I just EASILY had the BEST lunch I've ever eaten!  It was THAT good!\n\nThe chicken tortilla soup was out of this world...light and delicate...fresh and HOT!!!\nI had two fish tacos with no tortilla.  One was a regular fish taco and the other was a beer battered fish taco.l\n\nThe fire roasted salsa was EASILY the best salsa I have ever had, too!\n\nThis place is a serious gem!  I could go there every single day!\n\nThanks guys!!"
+
+    print("   ==> READING SPACY MODEL")
+    model_dir = "./workspace/models/er_model"
+    nlp = spacy.load(model_dir)
+
+    def get_entities(nlp_model, text):
+        """
+        Input nlp_model and text, retrieve a list of unique entities from the text.
+        """
+        doc = nlp_model(text)
+        entities = set()
+        for ent in doc.ents:
+            if ent.label_ == "PRODUCT":
+                entities.add(ent.text)
+        return list(entities)
+
+    print("entity example: ")
+    print("TEXT: ", example)
+    print("ENTITIES: \n") 
+    example_entity_list = get_entities(nlp, example)
+    print(example_entity_list)
+
+    bus = pd.read_csv("./workspace/end_to_end_test_set.csv")
+
+    business_ids_similar_stars = bus.business_id.unique()
+
+    rule = 'rule_2'
+
+    correlation_scores = []
+
+    print("RULE: ", rule)
+    for bus_id in tqdm(business_ids_similar_stars):
+        print("Running on restaurant ", bus_id, "...")
+        subset = bus[bus.business_id == bus_id]
+        
+        # only get reviews with enough amount of text
+        reviews_subset = [review for review in subset.text if len(review) < 400]
+
+        print("Number of Reviews left after subset length: ", len(reviews_subset))
+        
+        # get set of entities for this particular restaurant,
+        # and count how many reviews each entity have
+        entities_with_count = defaultdict(int) 
+        review_entities = [] # extract entities for each review
+        print("Extracting entities from each review...")
+        for review in tqdm(reviews_subset):
+            entities = get_entities(nlp, review)
+
+            # add this review as a count to an entity
+            for ent in entities:
+                entities_with_count[ent.lower()] += 1
+
+            review_entities.append(entities)
+            
+        # only grab entities that have enough reviews
+        print("Filtering entities to have enough reviews...")
+        entities_with_enough_reviews = []
+        threshold = 30
+        for key, value in entities_with_count.items():
+            if value >= threshold:
+                entities_with_enough_reviews.append(key)
+                
+        # TRUE RANKINGS CALCULATION
+        # for each entity, average ratings
+        true_rankings = defaultdict(list)
+
+        print("Calculating Yelp Star Rankings... ")
+        for entity in entities_with_enough_reviews:
+            true_rankings['entity'] += [entity]
+            entity_reviews = subset[subset.text.str.contains(entity, case=False)]
+            true_rankings['average_stars'] += [np.mean(entity_reviews.stars)]
+
+        true_rankings = pd.DataFrame(true_rankings)
+        
+        # PREDICTION RANKING CALCULATION
+        print("Calculating Prediction Rankings...")
+        # Filter entities of each review to be from the entities_with_enough_review set
+        entity_filter = set(entities_with_enough_reviews)
+
+        filtered_entities = []
+
+        for entities in review_entities:
+            filtered = []
+            for ent in entities:
+                ent = ent.lower()
+                if ent in entity_filter:
+                    filtered.append(ent)
+            filtered_entities.append(filtered)
+
+        # perform sentiment analysis for each review with filtered entities above
+        predicted_scores = defaultdict(list)
+
+        from .main import predict
+
+        print("Performing sentiment analysis for each review... ")
+        for i, review in enumerate(tqdm(reviews_subset)):
+            entities = filtered_entities[i]
+
+        #     print(review)
+
+            scores = predict(review)
+            print(scores)
+            #, entities, parser = parser, sentiment_package='vader', rule=rule)
+
+            # save results 
+            for entity, score in scores:
+                predicted_scores[entity] += [score]
+
+        # create rankings from scores
+        predicted_rankings = defaultdict(list)
+        for entity, scores in predicted_scores.items():
+            predicted_rankings['entity'] += [entity]
+            predicted_rankings['predicted_score'] += [np.mean(scores)]
+
+        predicted_rankings = pd.DataFrame(predicted_rankings)
+
+        #### may not be necessary to do these castings
+        predicted_rankings['entity'] = predicted_rankings['entity'].astype(str)
+        true_rankings['entity'] = true_rankings['entity'].astype(str)
+        ####
+        
+        full_rankings = true_rankings.merge(predicted_rankings, how='left').fillna(0)
+
+        # spearman correlation metric
+        print("Rankings result: ")
+        print(full_rankings)
+        
+        corr, pvalue = spearmanr(full_rankings.average_stars, full_rankings.predicted_score)
+        print("Spearman Correlation Score: ", corr)
+        correlation_scores.append(corr)
+    print("Final correlation score: ", np.mean(correlation_scores))
 
 
 def predict(input_data):
